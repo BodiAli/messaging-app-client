a9432781bdcb5c19dea6b1ca52e236785155ca86
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-10-29 10:48:35 +0300

    Feat: Render react-router routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import routes from "./routes/routes.js";
 import "./reset.css";
+
+const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLDivElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
