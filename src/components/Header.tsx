179ee01cdbd827ba2f88de1fa5e04ee0b47dc01b
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-11-21 21:22:37 +0200

    Create Header component to render header of website

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <div>
+          <img src="test" alt="Messaging App Logo" />
+        </div>
+        <h2>Messaging App</h2>
+      </div>
+    </header>
+  );
+}
