31bdbd4f2d62ab682a023635291fbffc9b11ffc9
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-11-22 01:36:02 +0200

    Feat: Export a setupStore method to create a new store instance for tests

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { createBrowserRouter, RouterProvider } from "react-router";
-import store from "./config/store.js";
+import setupStore from "./config/store.js";
 import routes from "./routes/routes.js";
 import "./global-styles/reset.css";
+
+const store = setupStore();
 
 const router = createBrowserRouter(routes);
 
