2b9eb7667bbf84d7f24b22ac99f7e595834aa212
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-11-21 21:23:28 +0200

    Feat: Creat test file for Header component

---
+++ src/tests/unit/components/Header.test.tsx
@@ -0,0 +1,14 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Header from "@/components/Header";
+
+describe("rendering Header component", () => {
+  it("should render website name and logo", () => {
+    expect.hasAssertions();
+
+    render(<Header />);
+
+    expect(screen.getByRole("heading", { name: "Messaging App", level: 2 })).toBeInTheDocument();
+    expect(screen.getByRole("img", { name: "Messaging App Logo" })).toBeInTheDocument();
+  });
+});
