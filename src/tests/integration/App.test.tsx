389280f629a368f3a7fc868ecb2edcd7a102f0e4
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-11-23 02:57:06 +0200

    Feat Mock fetch to match response sent by the server

--- src/tests/integration/App.test.tsx
+++ src/tests/integration/App.test.tsx
@@ -1,25 +1,55 @@
-import { screen, waitFor } from "@testing-library/react";
+import { waitFor } from "@testing-library/react";
+import fetchMock from "@fetch-mock/vitest";
 import { createMemoryRouter, RouterProvider } from "react-router";
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, beforeAll, beforeEach } from "vitest";
 import routes from "@/routes/routes";
 import renderWithProviders from "@/utils/test-utils";
+import type { User } from "@/types/modelsType";
+
+const serverRoute = `${import.meta.env.VITE_SERVER_URL}/auth/get-user`;
+
+const mockedUser: User = {
+  id: "mockId",
+  imageUrl: null,
+  isGuest: false,
+  lastSeen: new Date().toDateString(),
+  username: "mockUsername",
+};
 
 describe("render App component", () => {
-  it("should render Login page when user is not authenticated", () => {
-    expect.hasAssertions();
-
-    const router = createMemoryRouter(routes);
-
-    renderWithProviders(<RouterProvider router={router} />);
-
-    expect(router.state.location.pathname).toBe("/log-in");
+  beforeAll(() => {
+    fetchMock.mockGlobal();
   });
 
-  it("should render AppLayout component when user is authenticated", () => {
+  beforeEach(() => {
+    fetchMock.mockReset();
+  });
+
+  it("should redirect to login route when user is not authenticated", async () => {
     expect.hasAssertions();
+
+    fetchMock.get(serverRoute, 401);
 
     const router = createMemoryRouter(routes);
 
     renderWithProviders(<RouterProvider router={router} />);
+
+    await waitFor(() => {
+      expect(router.state.location.pathname).toBe("/log-in");
+    });
+  });
+
+  it("should continue to requested route when user is authenticated", async () => {
+    expect.hasAssertions();
+
+    fetchMock.get(serverRoute, { status: 200, body: mockedUser });
+
+    const router = createMemoryRouter(routes, { initialEntries: ["/friends"] });
+
+    renderWithProviders(<RouterProvider router={router} />);
+
+    await waitFor(() => {
+      expect(router.state.location.pathname).toBe("/friends");
+    });
   });
 });
