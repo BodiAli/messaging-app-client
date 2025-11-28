56918b44773c621087f0bd24a8cb5b4b9c799949
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-11-28 09:25:27 +0200

    Fix: Fetch user in async thunk to facilitate selecting user state from store

--- src/slices/apiSlice.ts
+++ src/slices/apiSlice.ts
@@ -1,17 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import type { User } from "../types/modelsType";
 
 const apiSlice = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_SERVER_URL }),
-  endpoints(build) {
-    return {
-      getUsers: build.query<User, undefined>({
-        query: () => "/auth/get-user",
-      }),
-    };
+  endpoints() {
+    return {};
   },
 });
-
-export const { useGetUsersQuery } = apiSlice;
 
 export default apiSlice;
