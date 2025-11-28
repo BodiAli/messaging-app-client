789f0cfe8f098dd4ff085206ead19948e94019d0
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-11-28 09:21:38 +0200

    Fix: Remove unnecessary fetch user query

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,7 @@
 import { Outlet } from "react-router";
 import Header from "../components/Header";
-import { useGetUsersQuery } from "../slices/apiSlice";
 
 export default function App() {
-  const res = useGetUsersQuery(undefined);
-
-  console.log(res);
-
   return (
     <>
       <Header />
