fe6267c72982a025797e5fc419d69e2bee320a34
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-11-30 15:44:41 +0200

    Feat: Render Loader component when fetching is in progress

--- src/app/ProtectedRoute.tsx
+++ src/app/ProtectedRoute.tsx
@@ -2,15 +2,14 @@ import Loader from "@/components/Loader";
 import { useEffect, useRef, type PropsWithChildren } from "react";
 import { Navigate } from "react-router";
 import { useAppDispatch, useAppSelector } from "./hooks";
-import { getUser, selectUserError } from "@/slices/authSlice";
+import { getUser, selectUserError, selectUserStatus } from "@/slices/authSlice";
 import { getJwtToken } from "@/services/localStorage";
-
-// TODO: check for userStatus instead of userError
 
 export default function ProtectedRoute({ children }: PropsWithChildren) {
   const jwtTokenRef = useRef(getJwtToken());
   const dispatch = useAppDispatch();
   const userError = useAppSelector(selectUserError);
+  const userStatus = useAppSelector(selectUserStatus);
 
   useEffect(() => {
     if (jwtTokenRef.current) {
@@ -20,7 +19,9 @@ export default function ProtectedRoute({ children }: PropsWithChildren) {
 
   if (!jwtTokenRef.current) return <Navigate to="/log-in" />;
 
-  if (userError) {
+  if (userStatus === "loading") return <Loader />;
+
+  if (userStatus === "failed" && userError) {
     if (userError === "unauthorized") {
       return <Navigate to="/log-in" />;
     }
