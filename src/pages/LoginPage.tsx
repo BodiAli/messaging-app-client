fe2a2e547c77f48b331859ba7f4dfd5f6c70a30a
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-12-01 11:23:28 +0200

    Feat: Initialize submitting login form logic

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,41 @@
+import { useLoginMutation } from "@/slices/authSlice";
+import { useNavigate } from "react-router";
+
 export default function LoginPage() {
+  const [login] = useLoginMutation();
+  const navigate = useNavigate();
+
   return (
     <main>
       <h1>Log in to your account</h1>
+
+      <form
+        aria-label="Login form"
+        onSubmit={(e) => {
+          const asyncHandler = async () => {
+            e.preventDefault();
+
+            const username = e.currentTarget.elements.username.value;
+            const password = e.currentTarget.elements.password.value;
+
+            await login({ username, password });
+
+            void navigate("/");
+          };
+
+          void asyncHandler();
+        }}
+      >
+        <label>
+          Username
+          <input type="text" name="username" />
+        </label>
+        <label>
+          Password
+          <input type="password" name="password" />
+        </label>
+        <button type="submit">Log in</button>
+      </form>
     </main>
   );
 }
