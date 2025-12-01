876643549ec56776c40745326aa9ebdc7fdcfc4b
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-12-01 11:24:20 +0200

    Fix: User concise serverUrl variable

--- src/slices/apiSlice.ts
+++ src/slices/apiSlice.ts
@@ -1,7 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import serverUrl from "@/utils/serverUrl";
 
 const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_SERVER_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: serverUrl }),
   endpoints() {
     return {};
   },
