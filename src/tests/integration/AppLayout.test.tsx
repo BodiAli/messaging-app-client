15152f8e9642790dac098f0df5c7db874b03ed72
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-12-02 11:54:26 +0200

    Feat: Create test file for AppLayout component

---
+++ src/tests/integration/AppLayout.test.tsx
@@ -0,0 +1,39 @@
+import { createMemoryRouter, RouterProvider } from "react-router";
+import { describe, it, expect } from "vitest";
+import renderWithProviders from "@/utils/test-utils";
+import AppLayout from "@/app/AppLayout";
+import { screen } from "@testing-library/react";
+
+describe("app-layout component", () => {
+  it("should render Header component and whatever component passed as children", () => {
+    expect.hasAssertions();
+
+    const router = createMemoryRouter(
+      [
+        {
+          Component: AppLayout,
+          children: [
+            {
+              path: "/test",
+              Component: () => <h3>Test</h3>,
+            },
+          ],
+        },
+      ],
+      {
+        initialEntries: ["/test"],
+      },
+    );
+
+    renderWithProviders(<RouterProvider router={router} />);
+
+    const headerComponentHeading = screen.getByRole("heading", {
+      level: 2,
+      name: "Messaging App",
+    });
+    const testHeading = screen.getByRole("heading", { level: 3, name: "Test" });
+
+    expect(headerComponentHeading).toBeInTheDocument();
+    expect(testHeading).toBeInTheDocument();
+  });
+});
