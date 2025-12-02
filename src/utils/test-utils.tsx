c5859be1a70610586f8703cd11e1c3d50a1a414f
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-12-02 08:11:32 +0200

    Feat: Fetch getUser on initialization to correspond how the user is fetched in the main entry point file

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -2,9 +2,16 @@ import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
 import type { PropsWithChildren, ReactElement } from "react";
 import setupStore from "@/config/store";
+import { apiSliceWithAuth } from "@/slices/authSlice";
+import { getJwtToken } from "@/services/localStorage";
 
 export default function renderWithProviders(ui: ReactElement) {
   const store = setupStore();
+  const jwtToken = getJwtToken();
+
+  if (jwtToken) {
+    void store.dispatch(apiSliceWithAuth.endpoints.getUser.initiate(jwtToken));
+  }
 
   const Wrapper = ({ children }: PropsWithChildren) => {
     return <Provider store={store}>{children}</Provider>;
