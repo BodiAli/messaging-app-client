88b27028adb23f9703a113b65b41dbc8a3aac532
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-12-02 11:53:01 +0200

    Feat: Allow App component to render main content of the app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,9 @@
 import { Outlet } from "react-router";
-import Header from "../components/Header";
 
 export default function App() {
   return (
     <>
-      <Header />
+      <p>Main page</p>
       <Outlet />
     </>
   );
