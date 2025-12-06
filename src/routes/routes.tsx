38cc5c2956ca7d5fd0a8f441ab088bf3fee9c087
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-12-06 14:09:51 +0200

    Fix: Add an ErrorBoundary to handle unexpected errors

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,22 +3,28 @@ import App from "@/app/App";
 import LoginPage from "@/pages/LoginPage";
 import ProtectedRoute from "@/app/ProtectedRoute";
 import AppLayout from "@/app/AppLayout";
+import ErrorBoundary from "@/components/ErrorBoundary";
 
 const routes: RouteObject[] = [
   {
-    Component: AppLayout,
+    ErrorBoundary: ErrorBoundary,
     children: [
       {
-        path: "/",
-        element: (
-          <ProtectedRoute>
-            <App />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: "/log-in",
-        Component: LoginPage,
+        Component: AppLayout,
+        children: [
+          {
+            path: "/",
+            element: (
+              <ProtectedRoute>
+                <App />
+              </ProtectedRoute>
+            ),
+          },
+          {
+            path: "/log-in",
+            Component: LoginPage,
+          },
+        ],
       },
     ],
   },
