23c87ca412ef8bcba96ecbb61d09c55b054a332c
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-12-06 14:11:01 +0200

    Change suite name && format function calls

--- src/tests/unit/components/Header.test.tsx
+++ src/tests/unit/components/Header.test.tsx
@@ -2,13 +2,17 @@ import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Header from "@/components/Header";
 
-describe("rendering Header component", () => {
+describe("header component", () => {
   it("should render website name and logo", () => {
     expect.hasAssertions();
 
     render(<Header />);
 
-    expect(screen.getByRole("heading", { name: "Messaging App", level: 2 })).toBeInTheDocument();
-    expect(screen.getByRole("img", { name: "Messaging App Logo" })).toBeInTheDocument();
+    expect(
+      screen.getByRole("heading", { name: "Messaging App", level: 2 }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("img", { name: "Messaging App Logo" }),
+    ).toBeInTheDocument();
   });
 });
