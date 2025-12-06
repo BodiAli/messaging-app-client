cc8052bfac1154043e41739f1ac189049b05e461
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-12-06 14:09:04 +0200

    Fix: Handle route error && change button to back in history

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -1,12 +1,16 @@
 import { Box, Button, Stack, Typography } from "@mui/material";
-import { useRouteError } from "react-router";
+import { isRouteErrorResponse, useNavigate, useRouteError } from "react-router";
 
 export default function ErrorBoundary() {
   const error = useRouteError();
+  const navigate = useNavigate();
+
   let errorMessage: string;
 
   if (error instanceof Error) {
     errorMessage = error.message;
+  } else if (isRouteErrorResponse(error)) {
+    errorMessage = error.statusText;
   } else {
     errorMessage = "Unknown error";
   }
@@ -19,14 +23,20 @@ export default function ErrorBoundary() {
       }}
     >
       <Stack alignItems={"center"} gap={4}>
-        <Typography variant="h3" component={"p"} textAlign={"center"}>
+        <Typography variant="h3" component={"h1"} textAlign={"center"}>
           Unexpected error occurred
         </Typography>
         <Typography fontSize={"2rem"} color="error">
           {errorMessage}
         </Typography>
-        <Button variant="contained" sx={{ fontSize: "1rem" }}>
-          Reload
+        <Button
+          variant="contained"
+          sx={{ fontSize: "1rem" }}
+          onClick={() => {
+            void navigate(-1);
+          }}
+        >
+          Go back
         </Button>
       </Stack>
     </Box>
