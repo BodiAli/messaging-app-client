05e31ceeea5f3813d36b7f316878172b7a8b7f2d
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-12-07 13:02:07 +0200

    Feat: Populate component with expected data

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,3 +1,13 @@
+import { Box, Typography } from "@mui/material";
+import { Link } from "react-router";
+
 export default function NotFoundPage() {
-  return <p>Not found</p>;
+  return (
+    <Box component="main">
+      <Typography variant="h1">404 Not Found</Typography>
+      <Typography>
+        Click <Link to="/">here</Link> to return to Home page
+      </Typography>
+    </Box>
+  );
 }
