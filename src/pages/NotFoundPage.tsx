7d4a09718c68fda9517f7dde9120d0204fd43ad5
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-12-07 14:51:58 +0200

    Feat: Add styles

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,13 +1,30 @@
-import { Box, Typography } from "@mui/material";
-import { Link } from "react-router";
+import { Box, Grid, Typography, Link } from "@mui/material";
+import { Link as RouterLink } from "react-router";
 
 export default function NotFoundPage() {
   return (
-    <Box component="main">
-      <Typography variant="h1">404 Not Found</Typography>
-      <Typography>
-        Click <Link to="/">here</Link> to return to Home page
-      </Typography>
+    <Box
+      component="main"
+      sx={{
+        paddingTop: 8,
+      }}
+    >
+      <Grid container direction={"column"} alignItems={"center"} spacing={3}>
+        <Grid>
+          <Typography fontWeight={400} variant="h1" color="error.dark">
+            404 Not Found
+          </Typography>
+        </Grid>
+        <Grid>
+          <Typography variant="h5" component={"p"}>
+            Click{" "}
+            <Link component={RouterLink} to="/">
+              here
+            </Link>{" "}
+            to return to Home page
+          </Typography>
+        </Grid>
+      </Grid>
     </Box>
   );
 }
