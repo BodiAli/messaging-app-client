d692aadf47ff4eec0f6b13318b8e7a95dce0d0ba
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-12-10 18:46:17 +0200

    Feat: render ThemeProvider to match rendering logic of the app

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,20 +1,30 @@
 import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import setupStore from "@/config/store";
 import { apiSliceWithAuth } from "@/slices/authSlice";
 import { getJwtToken } from "@/services/localStorage";
+import initializeTheme from "@/libs/theme";
 import type { PropsWithChildren, ReactElement } from "react";
 
 export default function renderWithProviders(ui: ReactElement) {
   const store = setupStore();
   const jwtToken = getJwtToken();
+  const theme = initializeTheme();
 
   if (jwtToken) {
     void store.dispatch(apiSliceWithAuth.endpoints.getUser.initiate(jwtToken));
   }
 
   const Wrapper = ({ children }: PropsWithChildren) => {
-    return <Provider store={store}>{children}</Provider>;
+    return (
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {children}
+        </ThemeProvider>
+      </Provider>
+    );
   };
 
   return render(ui, { wrapper: Wrapper });
