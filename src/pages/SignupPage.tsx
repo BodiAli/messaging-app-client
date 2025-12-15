f6e755e9ff2a63b4d8012b2e40c6aef5faa6e256
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-12-15 22:43:04 +0200

    Feat: render expected ui for signup page

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,3 +1,55 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Box, Button, TextField, Typography } from "@mui/material";
+
 export default function SignupPage() {
-  return <p>Signup page</p>;
+  const [formData, setFormData] = useState({
+    username: {
+      validation: {
+        empty: "Username cannot be empty.",
+        tooLong: "Username cannot exceed 100 characters.",
+      },
+      value: "kokowawa",
+    },
+  });
+
+  const [currentError, setCurrentError] = useState("");
+
+  return (
+    <Box component="main">
+      <Typography variant="h2" component="h1">
+        Create a new account
+      </Typography>
+      <Box component="form" aria-label="Sign up form">
+        <TextField
+          onChange={(e) => {
+            console.log(formData);
+
+            setFormData({
+              ...formData,
+              username: { ...formData.username, value: e.currentTarget.value },
+            });
+          }}
+          onBlur={(e) => {
+            const value = e.currentTarget.value;
+
+            if (value.trim().length === 0) {
+              setCurrentError(formData.username.validation.empty);
+            } else {
+              setCurrentError("");
+            }
+          }}
+          error={!!currentError}
+          helperText={currentError}
+          type="text"
+          label="Username"
+          required
+          value={formData.username.value}
+        />
+        <TextField type="password" label="Password" required />
+        <TextField type="password" label="Confirm password" required />
+        <Button type="submit">Create account</Button>
+      </Box>
+    </Box>
+  );
 }
