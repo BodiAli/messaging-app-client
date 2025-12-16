4ba12965e5fd0209b0c5316fb9fcc16f2702ce85
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-12-16 21:14:33 +0200

    Feat: validate form using react-hook-form

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,53 +1,60 @@
-import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { useForm, type SubmitHandler } from "react-hook-form";
 import { Box, Button, TextField, Typography } from "@mui/material";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { UserSignupSchema } from "@/libs/schemas";
+
+type FormValues = z.infer<typeof UserSignupSchema>;
 
 export default function SignupPage() {
-  const [formData, setFormData] = useState({
-    username: {
-      validation: {
-        empty: "Username cannot be empty.",
-        tooLong: "Username cannot exceed 100 characters.",
-      },
-      value: "kokowawa",
-    },
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(UserSignupSchema),
+    mode: "onBlur",
   });
 
-  const [currentError, setCurrentError] = useState("");
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    console.log(data);
+  };
 
   return (
     <Box component="main">
       <Typography variant="h2" component="h1">
         Create a new account
       </Typography>
-      <Box component="form" aria-label="Sign up form">
+      <Box
+        component="form"
+        aria-label="Sign up form"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <TextField
-          onChange={(e) => {
-            console.log(formData);
-
-            setFormData({
-              ...formData,
-              username: { ...formData.username, value: e.currentTarget.value },
-            });
-          }}
-          onBlur={(e) => {
-            const value = e.currentTarget.value;
-
-            if (value.trim().length === 0) {
-              setCurrentError(formData.username.validation.empty);
-            } else {
-              setCurrentError("");
-            }
-          }}
-          error={!!currentError}
-          helperText={currentError}
           type="text"
           label="Username"
+          autoComplete="username"
           required
-          value={formData.username.value}
+          {...register("username")}
+          error={!!errors.username}
+          helperText={errors.username?.message}
         />
-        <TextField type="password" label="Password" required />
-        <TextField type="password" label="Confirm password" required />
+        <TextField
+          type="password"
+          label="Password"
+          required
+          {...register("password")}
+          error={!!errors.password}
+          helperText={errors.password?.message}
+        />
+        <TextField
+          type="password"
+          label="Confirm password"
+          required
+          {...register("confirmPassword")}
+          error={!!errors.confirmPassword}
+          helperText={errors.confirmPassword?.message}
+        />
         <Button type="submit">Create account</Button>
       </Box>
     </Box>
