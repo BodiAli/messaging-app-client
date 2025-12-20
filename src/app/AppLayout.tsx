fa4d5851a0729b8a2bc71caac99557c95cecf87c
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-12-20 02:43:41 +0200

    Feat: update component path

--- src/app/AppLayout.tsx
+++ src/app/AppLayout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router";
 import Header from "@/components/Header";
 import { selectUserError, selectUserIsLoading } from "@/slices/authSlice";
-import Loader from "@/components/Loader";
+import Loader from "@/components/Loader/Loader";
 import { useAppSelector } from "./hooks";
 
 export default function AppLayout() {
