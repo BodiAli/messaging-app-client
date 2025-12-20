d2c6428b923acf1925dc8e7d80bd8b37346270e4
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-12-20 04:30:31 +0200

    Feat: make colors more accessible

--- src/libs/theme.ts
+++ src/libs/theme.ts
@@ -19,12 +19,12 @@ export default function initializeTheme() {
     palette: {
       primary: {
         main: "#4a4453",
-        light: "#b3a5c9",
+        light: "#847d90ff",
         dark: "#3a3542ff",
       },
       secondary: {
         main: "#f9d5cd",
-        dark: "#ad938eff",
+        dark: "#8B6A65",
       },
     },
   });
