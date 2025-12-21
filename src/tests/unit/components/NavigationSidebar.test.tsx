88560d1ad95bd49d04f49ab8996aff08cc1a1f42
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-12-21 23:12:31 +0200

    Feat: initialize testing sidebar navigation

---
+++ src/tests/unit/components/NavigationSidebar.test.tsx
@@ -0,0 +1,11 @@
+import { describe } from "vitest";
+
+describe("navigation-sidebar component", () => {
+  describe("friends icon link", () => {
+    it.todo("should render a friends icon link");
+
+    it.todo(
+      "should navigate to '/friends' path when the friends icon link is clicked",
+    );
+  });
+});
