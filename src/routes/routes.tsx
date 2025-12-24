367ae1e58bdae52b62b8d971b83de6c95d39ec19
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-12-24 03:50:29 +0200

    Feat: render expected pages based on expected routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -5,6 +5,10 @@ import AppLayout from "@/app/AppLayout";
 import ErrorBoundary from "@/components/ErrorBoundary";
 import NotFoundPage from "@/pages/NotFoundPage";
 import SignupPage from "@/pages/SignupPage";
+import FriendsPage from "@/pages/FriendsPage";
+import GroupsPage from "@/pages/GroupsPage";
+import NonFriendsPage from "@/pages/NonFriendsPage";
+import ProfilePage from "@/pages/ProfilePage";
 import type { RouteObject } from "react-router";
 
 const routes: RouteObject[] = [
@@ -21,6 +25,24 @@ const routes: RouteObject[] = [
                 <App />
               </ProtectedRoute>
             ),
+            children: [
+              {
+                path: "/friends",
+                Component: FriendsPage,
+              },
+              {
+                path: "/groups",
+                Component: GroupsPage,
+              },
+              {
+                path: "/non-friends",
+                Component: NonFriendsPage,
+              },
+              {
+                path: "/profile",
+                Component: ProfilePage,
+              },
+            ],
           },
           {
             path: "/log-in",
