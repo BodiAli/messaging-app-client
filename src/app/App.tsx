eadb7753e52d1cedf303e35f35d495ed69b5d6e2
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-12-24 03:49:08 +0200

    Feat: render navigation component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router";
+import NavigationSidebar from "@/components/NavigationSidebar";
 
 export default function App() {
   return (
     <>
-      <p>Main page</p>
+      <NavigationSidebar />
       <Outlet />
     </>
   );
