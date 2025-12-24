f20367ddd5c049b474834e54f74ec5dec66d9343
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-12-24 03:49:42 +0200

    Feat: render expected Link components

--- src/components/NavigationSidebar.tsx
+++ src/components/NavigationSidebar.tsx
@@ -1,11 +1,27 @@
 import { Link } from "react-router";
 import FriendsIcon from "@mui/icons-material/People";
+import GroupsIcon from "@mui/icons-material/Groups";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
 
 export default function NavigationSidebar() {
   return (
     <nav>
       <Link to="/friends">
-        <FriendsIcon /> Friends
+        <FriendsIcon />
+        Friends
+      </Link>
+      <Link to="/groups">
+        <GroupsIcon />
+        Groups
+      </Link>
+      <Link to="/non-friends">
+        <PermIdentityIcon />
+        Non friends
+      </Link>
+      <Link to="/profile">
+        <AccountBoxIcon />
+        Profile
       </Link>
     </nav>
   );
