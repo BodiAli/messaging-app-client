50761a8089fe732f9efe1318945e0e1c81e95ad7
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-12-25 09:03:26 +0200

    Feat: wrap layout in a flex container to allow sibling of drawer to take into account the drawer width

--- src/app/MainLayout.tsx
+++ src/app/MainLayout.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router";
+import { Box } from "@mui/material";
 import NavigationSidebar from "@/components/NavigationSidebar";
 
 export default function MainLayout() {
   return (
-    <>
+    <Box sx={{ display: "flex" }}>
       <NavigationSidebar />
       <Outlet />
-    </>
+    </Box>
   );
 }
