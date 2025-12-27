7af19fc883f5394ac029e09b63e09d069bec4672
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-12-27 12:07:52 +0200

    Feat: handle errors returned from server

--- src/pages/FriendsPage.tsx
+++ src/pages/FriendsPage.tsx
@@ -1,6 +1,20 @@
+import { Navigate } from "react-router";
 import { Box, Typography } from "@mui/material";
+import { useGetFriendsQuery } from "@/slices/friendsSlice";
+import handleUnexpectedError from "@/utils/handleUnexpectedError";
+import isUnauthorized from "@/utils/isUnauthorized";
 
 export default function FriendsPage() {
+  const { data, isError, error } = useGetFriendsQuery(undefined);
+
+  if (isError) {
+    if (isUnauthorized(error)) return <Navigate to="/log-in" />;
+
+    handleUnexpectedError(error);
+  }
+
+  console.log("FRIENDS PAGE DATA", data);
+
   return (
     <Box component="main">
       <Typography variant="h2">Your friends</Typography>
