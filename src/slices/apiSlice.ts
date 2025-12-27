600318ceb1cff4cec59fd707d81867f7f545f74b
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-12-27 12:08:28 +0200

    Feat: automatically set authorization header to specific endpoints

--- src/slices/apiSlice.ts
+++ src/slices/apiSlice.ts
@@ -1,8 +1,23 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import serverUrl from "@/utils/serverUrl";
+import { getJwtToken } from "@/services/localStorage";
 
 const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: serverUrl }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: serverUrl,
+    prepareHeaders(headers, { endpoint }) {
+      const jwtToken = getJwtToken();
+      if (
+        endpoint !== "getUser" &&
+        endpoint !== "login" &&
+        endpoint !== "signUp" &&
+        endpoint !== "loginAsGuest" &&
+        jwtToken
+      ) {
+        headers.set("Authorization", `Bearer ${jwtToken}`);
+      }
+    },
+  }),
   endpoints() {
     return {};
   },
