bfa870e0de1b595ae281380e425f649061e86a9a
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-12-29 03:15:50 +0200

    Feat: test navigation logic

--- src/tests/integration/FriendsPage.test.tsx
+++ src/tests/integration/FriendsPage.test.tsx
@@ -1,16 +1,27 @@
 import { screen } from "@testing-library/react";
-import { createMemoryRouter, RouterProvider } from "react-router";
+import { createMemoryRouter, RouterProvider, useParams } from "react-router";
 import { describe, it, beforeAll, afterEach, expect, beforeEach } from "vitest";
 import fetchMock, { manageFetchMockGlobally } from "@fetch-mock/vitest";
+import userEvent from "@testing-library/user-event";
 import routes from "@/routes/routes";
 import renderWithProviders from "@/utils/test-utils";
 import * as localStorageService from "@/services/localStorage";
 import serverUrl from "@/utils/serverUrl";
-import type { User } from "@/types/modelsType";
+import type { User, UserFriends } from "@/types/modelsType";
 
 vi.mock(import("@/pages/LoginPage"), () => {
   return {
     default: () => <h1>Log in page</h1>,
+  };
+});
+
+vi.mock(import("@/pages/MessageFriend"), () => {
+  return {
+    default: function MessageFriend() {
+      const { friendId } = useParams<{ friendId: string }>();
+
+      return <p>{friendId}</p>;
+    },
   };
 });
 
@@ -23,6 +34,23 @@ const mockedUser: User = {
   isGuest: false,
   lastSeen: new Date().toDateString(),
   username: "username",
+};
+
+const mockedUserFriends: UserFriends = {
+  friends: [
+    {
+      id: "idFriend1",
+      username: "usernameFriend1",
+      imageUrl: "imageUrl",
+      lastSeen: new Date().toISOString(),
+    },
+    {
+      id: "idFriend2",
+      username: "usernameFriend2",
+      imageUrl: "imageUrl",
+      lastSeen: new Date().toISOString(),
+    },
+  ],
 };
 
 describe("friends-page component", () => {
@@ -54,6 +82,10 @@ describe("friends-page component", () => {
   it("should render 'Your friends' heading", async () => {
     expect.hasAssertions();
 
+    fetchMock.get(getFriendsServerRoute, {
+      status: 200,
+      body: mockedUserFriends,
+    });
     renderFriendsPath();
 
     const heading = await screen.findByRole("heading", {
@@ -103,7 +135,28 @@ describe("friends-page component", () => {
     expect(logInPageHeading).toBeInTheDocument();
   });
 
-  it.todo("should render user friends", () => {
+  it("should navigate to the corresponding friend page when friend link is clicked", async () => {
     expect.hasAssertions();
+
+    fetchMock.get(getFriendsServerRoute, {
+      status: 200,
+      body: mockedUserFriends,
+    });
+    renderFriendsPath();
+
+    const friendsLinks = await screen.findAllByRole("link", {
+      name: /friend$/,
+    });
+
+    for (const friendLink of friendsLinks) {
+      await userEvent.click(friendLink);
+      const friendChatText = screen.getByText(/idFriend[1-2]/);
+
+      expect(friendChatText).toBeInTheDocument();
+    }
+
+    expect(friendsLinks).toHaveLength(2);
   });
+
+  it.todo("should render friends username, imageUrl, and lastSeen");
 });
