046aa837758a0a4f5a5be7e3a4257c5037e8388e
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-01-19 02:14:43 +0200

    Feat: test getting empty notifications && handle loading state

--- src/tests/integration/Notifications.test.tsx
+++ src/tests/integration/Notifications.test.tsx
@@ -1,8 +1,12 @@
-import { screen } from "@testing-library/react";
-import { describe, it, expect } from "vitest";
+import { screen, waitForElementToBeRemoved } from "@testing-library/react";
+import { describe, it, expect, beforeAll, afterEach } from "vitest";
 import userEvent from "@testing-library/user-event";
+import fetchMock, { manageFetchMockGlobally } from "@fetch-mock/vitest";
 import renderWithProviders from "@/utils/test-utils";
 import Notifications from "@/components/Notifications";
+import serverUrl from "@/utils/serverUrl";
+
+const notificationsRoute = `${serverUrl}/notifications/me`;
 
 vi.mock(import("@/app/MainLayout"), () => {
   return {
@@ -18,19 +22,38 @@ describe("notifications component", () => {
   const onClose = vi.fn<() => void>();
   const anchorElement = document.createElement("button");
 
-  it("should render Menu when open is true", () => {
+  function renderNotificationsComponent() {
+    return renderWithProviders(
+      <Notifications
+        open={true}
+        onClose={onClose}
+        anchorElement={anchorElement}
+      />,
+    );
+  }
+
+  beforeAll(() => {
+    fetchMock.mockGlobal();
+    manageFetchMockGlobally();
+  });
+
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+
+  it("should render Menu when open is true", async () => {
     expect.hasAssertions();
 
     renderWithProviders(
       <Notifications open anchorElement={anchorElement} onClose={onClose} />,
     );
 
-    const menu = screen.getByRole("menu");
+    const menu = await screen.findByRole("menu");
 
     expect(menu).toBeInTheDocument();
   });
 
-  it("should not render Menu when open is false", () => {
+  it("should not render Menu when open is false", async () => {
     expect.hasAssertions();
 
     renderWithProviders(
@@ -41,6 +64,7 @@ describe("notifications component", () => {
       />,
     );
 
+    await waitForElementToBeRemoved(screen.getByTestId("loader"));
     const menu = screen.queryByRole("menu");
 
     expect(menu).not.toBeInTheDocument();
@@ -57,12 +81,26 @@ describe("notifications component", () => {
       />,
     );
 
-    const menuBackdrop = screen.getByRole("presentation").firstChild;
+    const menuBackdrop = (await screen.findByRole("presentation")).firstChild;
     assertIsElement(menuBackdrop);
     await userEvent.click(menuBackdrop);
 
     expect(onClose).toHaveBeenCalledTimes(1);
   });
 
-  it.todo("should render 'No current notifications' when data length is 0");
+  it("should render 'No current notifications' when data length is 0", async () => {
+    expect.hasAssertions();
+
+    fetchMock.get(notificationsRoute, {
+      status: 200,
+      body: {
+        notifications: [],
+      },
+    });
+    renderNotificationsComponent();
+
+    const emptyText = await screen.findByText("No current notifications");
+
+    expect(emptyText).toBeInTheDocument();
+  });
 });
