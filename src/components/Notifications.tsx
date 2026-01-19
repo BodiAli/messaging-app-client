471cdb723cd58f3929042c560289794bd90d89fb
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-01-19 02:13:43 +0200

    Feat: initialize getting notifications

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -1,4 +1,6 @@
 import { Menu } from "@mui/material";
+import { useGetNotificationsQuery } from "@/slices/notificationsSlice";
+import Loader from "./Loader/Loader";
 
 export default function Notifications({
   open,
@@ -9,9 +11,14 @@ export default function Notifications({
   anchorElement: HTMLButtonElement;
   onClose: () => void;
 }) {
+  const { data, isLoading, error } = useGetNotificationsQuery(undefined);
+
+  if (isLoading) return <Loader />;
+
   return (
     <Menu onClose={onClose} open={open} anchorEl={anchorElement}>
       <li>Notifications</li>
+      {data?.notifications.length === 0 && <p>No current notifications</p>}
     </Menu>
   );
 }
