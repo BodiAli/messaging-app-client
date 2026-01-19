7786aa2362a8849a7f99ebc7c30b5adbd37b959b
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-01-19 20:16:16 +0200

    Feat: test and inplement handling unexpected errors

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -1,5 +1,7 @@
 import { Menu } from "@mui/material";
 import { useGetNotificationsQuery } from "@/slices/notificationsSlice";
+import isUnauthorized from "@/utils/isUnauthorized";
+import handleUnexpectedError from "@/utils/handleUnexpectedError";
 import Loader from "./Loader/Loader";
 
 export default function Notifications({
@@ -11,9 +13,14 @@ export default function Notifications({
   anchorElement: HTMLButtonElement;
   onClose: () => void;
 }) {
-  const { data, isLoading, error } = useGetNotificationsQuery(undefined);
+  const { data, isLoading, error, isError } =
+    useGetNotificationsQuery(undefined);
 
   if (isLoading) return <Loader />;
+
+  if (isError && !isUnauthorized(error)) {
+    handleUnexpectedError(error);
+  }
 
   return (
     <Menu onClose={onClose} open={open} anchorEl={anchorElement}>
--- src/tests/integration/Notifications.test.tsx
+++ src/tests/integration/Notifications.test.tsx
@@ -1,10 +1,12 @@
 import { screen, waitForElementToBeRemoved } from "@testing-library/react";
+import { createMemoryRouter, RouterProvider } from "react-router";
 import { describe, it, expect, beforeAll, afterEach } from "vitest";
 import userEvent from "@testing-library/user-event";
 import fetchMock, { manageFetchMockGlobally } from "@fetch-mock/vitest";
 import renderWithProviders from "@/utils/test-utils";
 import Notifications from "@/components/Notifications";
 import serverUrl from "@/utils/serverUrl";
+import ErrorBoundary from "@/components/ErrorBoundary";
 
 const notificationsRoute = `${serverUrl}/notifications/me`;
 
@@ -44,6 +46,12 @@ describe("notifications component", () => {
   it("should render Menu when open is true", async () => {
     expect.hasAssertions();
 
+    fetchMock.get(notificationsRoute, {
+      status: 200,
+      body: {
+        notifications: [],
+      },
+    });
     renderWithProviders(
       <Notifications open anchorElement={anchorElement} onClose={onClose} />,
     );
@@ -56,6 +64,12 @@ describe("notifications component", () => {
   it("should not render Menu when open is false", async () => {
     expect.hasAssertions();
 
+    fetchMock.get(notificationsRoute, {
+      status: 200,
+      body: {
+        notifications: [],
+      },
+    });
     renderWithProviders(
       <Notifications
         open={false}
@@ -73,6 +87,12 @@ describe("notifications component", () => {
   it("should call onClose function when clicking away", async () => {
     expect.hasAssertions();
 
+    fetchMock.get(notificationsRoute, {
+      status: 200,
+      body: {
+        notifications: [],
+      },
+    });
     renderWithProviders(
       <Notifications
         open={true}
@@ -86,6 +106,57 @@ describe("notifications component", () => {
     await userEvent.click(menuBackdrop);
 
     expect(onClose).toHaveBeenCalledTimes(1);
+  });
+
+  it("should render Loader component when data is loading", () => {
+    expect.hasAssertions();
+
+    fetchMock.get(notificationsRoute, {
+      status: 200,
+      body: {
+        notifications: [],
+      },
+    });
+    renderNotificationsComponent();
+
+    const loader = screen.getByTestId("loader");
+
+    expect(loader).toBeVisible();
+  });
+
+  it("should throw error when an unexpected error occurs", async () => {
+    expect.hasAssertions();
+
+    vi.spyOn(console, "error").mockImplementation(() => null);
+    fetchMock.get(notificationsRoute, {
+      status: 500,
+      body: {
+        error: "Server error",
+      },
+    });
+
+    const router = createMemoryRouter([
+      {
+        ErrorBoundary: ErrorBoundary,
+        children: [
+          {
+            path: "/",
+            element: (
+              <Notifications
+                onClose={onClose}
+                open
+                anchorElement={anchorElement}
+              />
+            ),
+          },
+        ],
+      },
+    ]);
+    renderWithProviders(<RouterProvider router={router} />);
+
+    const errorMessage = await screen.findByText("Server error");
+
+    expect(errorMessage).toBeInTheDocument();
   });
 
   it("should render 'No current notifications' when data length is 0", async () => {
