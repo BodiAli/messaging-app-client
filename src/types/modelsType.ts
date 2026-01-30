4b5cb1e941ae8157c6441de104479e7753d7bb01
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-01-30 07:05:29 +0200

    Feat: export type of server response when fetching messages

--- src/types/modelsType.ts
+++ src/types/modelsType.ts
@@ -1,4 +1,4 @@
-import type { User as PrismaUser } from "prismaTypes";
+import type { User as PrismaUser, Message as PrismaMessage } from "prismaTypes";
 
 export interface User extends Omit<PrismaUser, "lastSeen" | "password"> {
   lastSeen: string;
@@ -9,3 +9,7 @@ export type Friend = Omit<User, "isGuest">;
 export interface UserFriends {
   friends: Friend[];
 }
+
+export type Messages = (Omit<PrismaMessage, "createdAt"> & {
+  createdAt: string;
+})[];
