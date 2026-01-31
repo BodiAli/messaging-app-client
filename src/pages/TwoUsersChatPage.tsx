0ff3478cae0b87e02cc77ec237aaa91001f79fea
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-01-31 12:27:05 +0200

    Feat: handle 404 error

--- src/pages/TwoUsersChatPage.tsx
+++ src/pages/TwoUsersChatPage.tsx
@@ -1,22 +1,39 @@
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
+import { useSnackbar } from "notistack";
+import { useEffect } from "react";
 import { useGetTwuUsersMessagesQuery } from "@/slices/messagesSlice";
 import handleUnexpectedError from "@/utils/handleUnexpectedError";
 
-function assert(val: unknown): asserts val {
-  if (!val) {
-    throw new Error("Value is undefined");
+function assert(userId: unknown): asserts userId {
+  if (!userId) {
+    throw new Error("userId is undefined");
   }
 }
 
 export default function TwoUsersChatPage() {
-  const { userId } = useParams<{ userId: string }>();
+  const { userId } = useParams<"userId">();
   assert(userId);
-  const { isError, error, isLoading } = useGetTwuUsersMessagesQuery(userId);
+  const navigate = useNavigate();
+  const { enqueueSnackbar } = useSnackbar();
+  const { data, isError, error, isLoading } =
+    useGetTwuUsersMessagesQuery(userId);
+
+  useEffect(() => {
+    if (isError) {
+      if ("data" in error) {
+        if (error.status === 404) {
+          void navigate(-1);
+          enqueueSnackbar(error.data.errors[0].message, {
+            variant: "error",
+          });
+          return;
+        }
+      }
+      handleUnexpectedError(error);
+    }
+  }, [error, isError, enqueueSnackbar, navigate]);
 
   if (isLoading) return <p>Loading...</p>;
-  if (isError) {
-    handleUnexpectedError(error);
-  }
 
   return <p>asd</p>;
 }
