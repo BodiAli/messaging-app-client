0033f93a61ca0e2c4e538e59b1b6102ad9ad9005
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-02-01 14:01:42 +0200

    Feat: test rendering friendship action logic

--- src/tests/integration/UserChat.test.tsx
+++ src/tests/integration/UserChat.test.tsx
@@ -1,10 +1,28 @@
 import { createRoutesStub } from "react-router";
-import { describe, it, expect } from "vitest";
-import { screen, within } from "@testing-library/react";
+import { describe, it, expect, beforeAll, afterEach, beforeEach } from "vitest";
+import {
+  screen,
+  waitForElementToBeRemoved,
+  within,
+} from "@testing-library/react";
+import fetchMock, { manageFetchMockGlobally } from "@fetch-mock/vitest";
 import userEvent from "@testing-library/user-event";
 import renderWithProviders from "@/utils/test-utils";
 import UserChat from "@/components/UserChat";
-import type { ChatData } from "@/types/modelsType";
+import * as localStorageService from "@/services/localStorage";
+import serverUrl from "@/utils/serverUrl";
+import AppLayout from "@/app/AppLayout";
+import type { ChatData, User } from "@/types/modelsType";
+
+const getUserServerRoute = `${serverUrl}/auth/get-user`;
+
+const mockUser: User = {
+  id: "userAId",
+  username: "userA",
+  imageUrl: null,
+  isGuest: false,
+  lastSeen: new Date().toISOString(),
+};
 
 const mockChatData: ChatData = {
   messages: [
@@ -32,36 +50,53 @@ const mockChatData: ChatData = {
     lastSeen: new Date().toISOString(),
     username: "userB",
   },
+  friendRequestStatus: null,
 };
 
 describe("user-chat component", () => {
-  function renderUserChat({ isFriend }: { isFriend: boolean }) {
+  function renderUserChat(chatData: ChatData = mockChatData) {
     const Stub = createRoutesStub([
       {
         path: "/",
-        Component: () => (
-          <UserChat chatData={mockChatData} isFriend={isFriend} />
-        ),
+        Component: AppLayout,
+        children: [
+          {
+            path: "/friends/:userId",
+            Component: () => <UserChat chatData={chatData} />,
+          },
+        ],
       },
     ]);
 
-    return renderWithProviders(<Stub />);
+    return renderWithProviders(<Stub initialEntries={["/friends/userBId"]} />);
   }
+
+  beforeAll(() => {
+    fetchMock.mockGlobal();
+    manageFetchMockGlobally();
+  });
+
+  beforeEach(() => {
+    vi.spyOn(localStorageService, "getJwtToken").mockReturnValue("jwtToken");
+    fetchMock.get(getUserServerRoute, {
+      status: 200,
+      body: {
+        user: mockUser,
+      },
+    });
+  });
+
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
 
   describe("back button navigation", () => {
     describe("given rendering back button", () => {
-      it("should render a back button with an icon", () => {
+      it("should render a back button with an icon", async () => {
         expect.hasAssertions();
 
-        const Stub = createRoutesStub([
-          {
-            path: "/friends/:userId",
-            Component: () => <UserChat chatData={mockChatData} isFriend />,
-          },
-        ]);
-        renderWithProviders(<Stub initialEntries={["/friends/userId"]} />);
-
-        const backButton = screen.getByRole("button", {
+        renderUserChat();
+        const backButton = await screen.findByRole("button", {
           name: "back",
         });
         const backIcon = within(backButton).getByTestId("ArrowBackIcon");
@@ -77,19 +112,24 @@ describe("user-chat component", () => {
 
         const Stub = createRoutesStub([
           {
-            path: "/friends/:userId",
-            Component: () => <UserChat chatData={mockChatData} isFriend />,
-          },
-          {
-            path: "/test",
-            Component: () => <p>Test</p>,
+            Component: AppLayout,
+            children: [
+              {
+                path: "/friends/:userId",
+                Component: () => <UserChat chatData={mockChatData} />,
+              },
+              {
+                path: "/test",
+                Component: () => <p>Test</p>,
+              },
+            ],
           },
         ]);
         renderWithProviders(
-          <Stub initialEntries={["/test", "/friends/userId"]} />,
+          <Stub initialEntries={["/test", "/friends/userBId"]} />,
         );
 
-        const backButton = screen.getByRole("button", {
+        const backButton = await screen.findByRole("button", {
           name: "back",
         });
         await userEvent.click(backButton);
@@ -100,14 +140,14 @@ describe("user-chat component", () => {
     });
   });
 
-  describe("rendering messages data", () => {
+  describe("messages data", () => {
     describe("given name of the current user being chatted with", () => {
-      it("should render the name of the current user", () => {
+      it("should render the name of the current user", async () => {
         expect.hasAssertions();
 
-        renderUserChat({ isFriend: true });
+        renderUserChat();
 
-        const currentUser = screen.getByRole("heading", {
+        const currentUser = await screen.findByRole("heading", {
           name: `Chatting with ${mockChatData.user.username}`,
         });
 
@@ -115,31 +155,83 @@ describe("user-chat component", () => {
       });
     });
 
-    describe("give user is a friend", () => {
-      it("should not render a button to add as a friend if user is friend", () => {
+    describe.todo("given imageUrl");
+
+    describe("given user is a friend", () => {
+      it("should not render a button to add as a friend if user is friend", async () => {
         expect.hasAssertions();
 
-        renderUserChat({ isFriend: true });
+        renderUserChat({
+          ...mockChatData,
+          friendRequestStatus: { type: "ACCEPTED", senderId: "userBId" },
+        });
 
+        await waitForElementToBeRemoved(screen.getByTestId("loader"));
         const addFriend = screen.queryByRole("button", {
           name: "Add as a friend",
         });
 
         expect(addFriend).not.toBeInTheDocument();
       });
+
+      it.todo(
+        "if friend is last seen within the last 5 minutes it should render an online badge",
+      );
+
+      it.todo(
+        "if friend is not last seen within the last 5 minutes it should not render an online badge",
+      );
     });
 
     describe("given user is not a friend", () => {
-      it("should render an add as a friend button", () => {
+      it("should render an add as a friend button", async () => {
         expect.hasAssertions();
 
-        renderUserChat({ isFriend: false });
+        renderUserChat();
 
-        const addFriend = screen.getByRole("button", {
+        const addFriend = await screen.findByRole("button", {
           name: "Add as a friend",
         });
 
         expect(addFriend).toBeInTheDocument();
+      });
+    });
+
+    describe("given a friend request is already sent by the user being chatted with", () => {
+      it("should render an accept or decline buttons", async () => {
+        expect.hasAssertions();
+
+        renderUserChat({
+          ...mockChatData,
+          friendRequestStatus: { type: "PENDING", senderId: "userBId" },
+        });
+
+        const acceptRequestButton = await screen.findByRole("button", {
+          name: "Accept",
+        });
+        const declineRequestButton = await screen.findByRole("button", {
+          name: "Decline",
+        });
+
+        expect(acceptRequestButton).toBeInTheDocument();
+        expect(declineRequestButton).toBeInTheDocument();
+      });
+    });
+
+    describe("given a friend request is already sent by the current user", () => {
+      it("should render a 'Cancel request' button", async () => {
+        expect.hasAssertions();
+
+        renderUserChat({
+          ...mockChatData,
+          friendRequestStatus: { type: "PENDING", senderId: mockUser.id },
+        });
+
+        const cancelButton = await screen.findByRole("button", {
+          name: "Cancel request",
+        });
+
+        expect(cancelButton).toBeInTheDocument();
       });
     });
   });
