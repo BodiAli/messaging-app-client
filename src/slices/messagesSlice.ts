c10a7d21655d1d9cad3901e2067499aa2eb89628
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-02-08 16:35:24 +0200

    Feat: implement sending message query

--- src/slices/messagesSlice.ts
+++ src/slices/messagesSlice.ts
@@ -4,11 +4,24 @@ import type { ChatData } from "@/types/modelsType";
 const apiSliceWithMessages = apiSlice.injectEndpoints({
   endpoints(build) {
     return {
-      getTwuUsersMessages: build.query<ChatData, string>({
+      getTwoUsersMessages: build.query<ChatData, string>({
         query: (userId) => `/users/${userId}/messages`,
+      }),
+      sendMessage: build.mutation<
+        undefined,
+        { userId: string; formData: FormData }
+      >({
+        query: ({ userId, formData }) => {
+          return {
+            url: `/users/${userId}/messages`,
+            method: "POST",
+            body: formData,
+          };
+        },
       }),
     };
   },
 });
 
-export const { useGetTwuUsersMessagesQuery } = apiSliceWithMessages;
+export const { useGetTwoUsersMessagesQuery, useSendMessageMutation } =
+  apiSliceWithMessages;
