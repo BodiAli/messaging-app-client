3ee076d765323bc6614887e4d87db987754c4c73
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-03-22 07:59:18 +0200

    Feat: render groups returned by the server and handle any errors

--- src/pages/GroupsPage.tsx
+++ src/pages/GroupsPage.tsx
@@ -1,3 +1,36 @@
+import { Box, Typography, Link } from "@mui/material";
+import { Outlet, Link as RouterLink } from "react-router";
+import { useGetGroupsQuery } from "@/slices/groupsSlice";
+import handleUnexpectedError from "@/utils/handleUnexpectedError";
+
 export default function GroupsPage() {
-  return <p>GROUPS</p>;
+  const {
+    data = { groups: [] },
+    isLoading,
+    isError,
+    error,
+  } = useGetGroupsQuery(undefined);
+
+  if (isLoading) return <p>Loading...</p>;
+
+  if (isError) {
+    handleUnexpectedError(error);
+  }
+
+  return (
+    <Box component={"main"}>
+      {data.groups.length === 0 ? (
+        <Typography>You are not in any groups</Typography>
+      ) : (
+        data.groups.map((group) => {
+          return (
+            <Link key={group.id} to={group.id} component={RouterLink}>
+              {group.name}
+            </Link>
+          );
+        })
+      )}
+      <Outlet />
+    </Box>
+  );
 }
