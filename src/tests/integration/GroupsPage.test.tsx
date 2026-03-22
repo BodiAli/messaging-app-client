0722165d71557496d07efb0bb70a3f121814ae41
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-03-22 08:01:18 +0200

    Feat: create implement tests for rendering and navigating inside GroupsPage component

---
+++ src/tests/integration/GroupsPage.test.tsx
@@ -0,0 +1,169 @@
+import { screen } from "@testing-library/react";
+import { describe, it, expect, vi, beforeAll, afterEach, assert } from "vitest";
+import fetchMock, { manageFetchMockGlobally } from "@fetch-mock/vitest";
+import { createMemoryRouter, RouterProvider } from "react-router";
+import userEvent from "@testing-library/user-event";
+import renderWithProviders from "@/utils/test-utils";
+import GroupsPage from "@/pages/GroupsPage";
+import ErrorBoundary from "@/components/ErrorBoundary";
+import type { GroupChat } from "@/types/modelsType";
+import GroupChatPage from "@/pages/GroupChatPage";
+
+const serverUserGroupsRoute = "/users/me/groups";
+
+vi.mock(import("@/pages/GroupChatPage"), () => {
+  return {
+    default: () => <p>group chat page</p>,
+  };
+});
+
+describe("groups-page-component", () => {
+  const renderGroupsRoute = () => {
+    const router = createMemoryRouter(
+      [
+        {
+          ErrorBoundary: ErrorBoundary,
+          children: [
+            {
+              path: "/groups",
+              Component: GroupsPage,
+              children: [
+                {
+                  path: ":groupId",
+                  Component: GroupChatPage,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+      { initialEntries: ["/groups"] },
+    );
+
+    renderWithProviders(<RouterProvider router={router} />);
+  };
+
+  const mockGroupsResponse: {
+    groups: GroupChat[];
+  } = {
+    groups: [
+      {
+        adminId: "adminId1",
+        createdAt: "2020-01-01-T01:30:30",
+        id: "groupId1",
+        name: "groupName1",
+      },
+      {
+        adminId: "adminId2",
+        createdAt: "2020-01-01-T01:30:30",
+        id: "groupId2",
+        name: "groupName2",
+      },
+    ],
+  };
+
+  beforeAll(() => {
+    fetchMock.mockGlobal();
+    manageFetchMockGlobally();
+  });
+
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+
+  describe("rendering group cards", () => {
+    it("should render ErrorBoundary when an unexpected error occurs", async () => {
+      expect.hasAssertions();
+
+      vi.spyOn(console, "error").mockImplementation(() => null);
+      fetchMock.get(serverUserGroupsRoute, {
+        status: 500,
+        body: {
+          error: "Server Error!",
+        },
+      });
+      renderGroupsRoute();
+
+      const errorBoundaryHeading = await screen.findByRole("heading", {
+        name: "Unexpected error occurred",
+      });
+      const errorText = screen.getByText("Server Error!");
+
+      expect(errorBoundaryHeading).toBeInTheDocument();
+      expect(errorText).toBeInTheDocument();
+    });
+
+    it("should render 'You are not in any groups' when groups are an empty array", async () => {
+      expect.hasAssertions();
+
+      fetchMock.get(serverUserGroupsRoute, {
+        status: 200,
+        body: {
+          groups: [],
+        },
+      });
+      renderGroupsRoute();
+
+      const emptyText = await screen.findByText("You are not in any groups");
+
+      expect(emptyText).toBeInTheDocument();
+    });
+
+    it("should render groups data", async () => {
+      expect.hasAssertions();
+
+      fetchMock.get(serverUserGroupsRoute, {
+        status: 200,
+        body: mockGroupsResponse,
+      });
+      renderGroupsRoute();
+
+      const groups = await screen.findAllByRole("link");
+      assert(groups[0]);
+      assert(groups[1]);
+
+      expect(groups[0]).toHaveTextContent("groupName1");
+      expect(groups[1]).toHaveTextContent("groupName2");
+    });
+  });
+
+  describe("navigating between group links", () => {
+    it("should navigate to first group route when clicked", async () => {
+      expect.hasAssertions();
+
+      fetchMock.get(serverUserGroupsRoute, {
+        status: 200,
+        body: mockGroupsResponse,
+      });
+      renderGroupsRoute();
+
+      const groups = await screen.findAllByRole<HTMLAnchorElement>("link");
+      assert(groups[0]);
+      const firstGroup = groups[0];
+      await userEvent.click(firstGroup);
+      const groupChatPageElement = screen.getByText("group chat page");
+
+      expect(firstGroup.pathname).toBe("/groups/groupId1");
+      expect(groupChatPageElement).toBeInTheDocument();
+    });
+
+    it("should navigate to second group route when clicked", async () => {
+      expect.hasAssertions();
+
+      fetchMock.get(serverUserGroupsRoute, {
+        status: 200,
+        body: mockGroupsResponse,
+      });
+      renderGroupsRoute();
+
+      const groups = await screen.findAllByRole<HTMLAnchorElement>("link");
+      assert(groups[1]);
+      const secondGroup = groups[1];
+      await userEvent.click(secondGroup);
+      const groupChatPageElement = screen.getByText("group chat page");
+
+      expect(secondGroup.pathname).toBe("/groups/groupId2");
+      expect(groupChatPageElement).toBeInTheDocument();
+    });
+  });
+});
