796648b09e11acb0d26dc91d8133ce6a731edd20
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-03-24 05:11:30 +0200

    Feat: style card component

--- src/pages/GroupsPage.tsx
+++ src/pages/GroupsPage.tsx
@@ -1,7 +1,22 @@
-import { Box, Typography, Link } from "@mui/material";
-import { Outlet, Link as RouterLink } from "react-router";
+import {
+  Box,
+  Typography,
+  Card,
+  CardHeader,
+  Skeleton,
+  Stack,
+  CardActionArea,
+  type CardActionAreaProps,
+} from "@mui/material";
+import { Outlet, NavLink } from "react-router";
 import { useGetGroupsQuery } from "@/slices/groupsSlice";
 import handleUnexpectedError from "@/utils/handleUnexpectedError";
+
+const CustomCardActionArea = (
+  props: CardActionAreaProps<"button", { to: string }>,
+) => {
+  return <CardActionArea {...props} />;
+};
 
 export default function GroupsPage() {
   const {
@@ -11,26 +26,57 @@ export default function GroupsPage() {
     error,
   } = useGetGroupsQuery(undefined);
 
-  if (isLoading) return <p>Loading...</p>;
-
   if (isError) {
     handleUnexpectedError(error);
   }
 
   return (
-    <Box component={"main"}>
-      <Typography variant="h2">Your groups</Typography>
-      {data.groups.length === 0 ? (
-        <Typography>You are not in any groups</Typography>
-      ) : (
-        data.groups.map((group) => {
-          return (
-            <Link key={group.id} to={group.id} component={RouterLink}>
-              {group.name}
-            </Link>
-          );
-        })
-      )}
+    <Box
+      component={"main"}
+      sx={{ display: "grid", gridTemplateColumns: "0.3fr 1fr", flex: 1 }}
+    >
+      <Box component={"section"}>
+        <Typography variant="h2" sx={{ paddingY: 3, textAlign: "center" }}>
+          Your groups
+        </Typography>
+        {isLoading ? (
+          <Card>
+            <CardHeader
+              avatar={
+                <Skeleton
+                  height={40}
+                  width={40}
+                  variant="circular"
+                  animation="wave"
+                />
+              }
+              title={<Skeleton animation="wave" />}
+            />
+          </Card>
+        ) : data.groups.length === 0 ? (
+          <Typography>You are not in any groups</Typography>
+        ) : (
+          <Stack>
+            {data.groups.map((group) => {
+              return (
+                <Card key={group.id}>
+                  <CustomCardActionArea
+                    LinkComponent={NavLink}
+                    to={group.id}
+                    sx={{
+                      "&.active": {
+                        backgroundColor: "action.selected",
+                      },
+                    }}
+                  >
+                    <CardHeader title={group.name} />
+                  </CustomCardActionArea>
+                </Card>
+              );
+            })}
+          </Stack>
+        )}
+      </Box>
       <Outlet />
     </Box>
   );
