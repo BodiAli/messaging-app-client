146fe0beb2cffaa0f96f7bb2240fbfe5d0a88bb5
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-03-30 08:44:07 +0200

    Feat: implement form to create a new group

--- src/components/GroupsPageIndex.tsx
+++ src/components/GroupsPageIndex.tsx
@@ -1,10 +1,64 @@
-import { Box } from "@mui/material";
-import { Link } from "react-router";
+import { Box, Button, List, TextField, Typography } from "@mui/material";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { GroupSchema } from "@/libs/schemas";
+import { useCreateGroupMutation } from "@/slices/groupsSlice";
+import { isClientError, isFetchBaseQueryError } from "@/types/apiResponseTypes";
+import handleClientError from "@/utils/handleClientError";
+import type { z } from "zod";
+import type { ReactElement } from "react";
+
+type FormFields = z.infer<typeof GroupSchema>;
 
 export default function GroupsPageIndex() {
+  const [createGroup, { isError, error }] = useCreateGroupMutation();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(GroupSchema),
+  });
+  let errorsList: ReactElement[] | null = null;
+
+  if (isError) {
+    if (isFetchBaseQueryError(error) && isClientError(error.data)) {
+      errorsList = handleClientError(error.data);
+    }
+  }
+
+  const onSubmit: SubmitHandler<FormFields> = async (data) => {
+    await createGroup(data.groupName);
+  };
+
   return (
     <Box>
-      <Link to={"create-group"}>Create new group</Link>;
+      <Typography variant="h1">Create a new group</Typography>
+      <Box
+        component={"form"}
+        aria-label="create new group"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        {errorsList && (
+          <List
+            sx={{
+              listStyleType: "disc",
+              listStylePosition: "inside",
+              color: (theme) => theme.palette.error.main,
+            }}
+          >
+            {errorsList}
+          </List>
+        )}
+        <TextField
+          label="Group name"
+          required
+          {...register("groupName")}
+          error={!!errors.groupName}
+          helperText={errors.groupName?.message}
+        />
+        <Button type="submit">Create group</Button>
+      </Box>
     </Box>
   );
 }
