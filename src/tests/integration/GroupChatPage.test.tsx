1fcab1e0395c032b6a0117bb65d2f29d41dfcd6d
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-04-01 10:56:11 +0200

    Feat: test rendering group messages data logic

--- src/tests/integration/GroupChatPage.test.tsx
+++ src/tests/integration/GroupChatPage.test.tsx
@@ -1,23 +1,28 @@
-import { describe, it, vi, beforeAll, afterEach, expect } from "vitest";
-import { screen, waitForElementToBeRemoved } from "@testing-library/react";
-import * as notistack from "notistack";
+import {
+  describe,
+  it,
+  vi,
+  beforeAll,
+  afterEach,
+  expect,
+  beforeEach,
+} from "vitest";
+import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import fetchMock, { manageFetchMockGlobally } from "@fetch-mock/vitest";
 import { createMemoryRouter, Outlet, RouterProvider } from "react-router";
+import * as notistack from "notistack";
 import routes from "@/routes/routes";
 import renderWithProviders from "@/utils/test-utils";
+import * as jwtTokenService from "@/services/localStorage";
 import type { ApiClientError } from "@/types/apiResponseTypes";
+import type { GroupMessages, User } from "@/types/modelsType";
 
-const serverGetGroupChat = "/users/me/groups/groupId/messages";
+const serverGroupMessagesRoute = "/users/me/groups/Test-GroupId/messages";
 
-vi.mock(import("@/app/ProtectedRoute"), () => {
+vi.mock(import("@/components/Header"), () => {
   return {
-    default: ({ children }) => children,
-  };
-});
-
-vi.mock(import("@/app/AppLayout"), () => {
-  return {
-    default: () => <Outlet />,
+    default: () => <p>Header Component</p>,
   };
 });
 
@@ -27,16 +32,84 @@ vi.mock(import("@/app/MainLayout"), () => {
   };
 });
 
+vi.mock(import("@/pages/IndexPage"), () => {
+  return {
+    default: () => <p>Index Page</p>,
+  };
+});
+
 vi.mock(import("@/pages/GroupsPage"), () => {
   return {
     default: () => <Outlet />,
   };
 });
 
+vi.mock(import("@/pages/GroupDetails"), () => {
+  return {
+    default: () => <p>Group Details Page</p>,
+  };
+});
+
 describe("group-chat-page component", () => {
+  const mockGroupMessages: GroupMessages = {
+    group: {
+      createdAt: "2020-01-01T01:30:00Z",
+      id: "Test-GroupId",
+      name: "Test-GroupName",
+    },
+    messages: [
+      {
+        content: "Test-MessageContent1",
+        createdAt: "2020-01-01T01:30:00Z",
+        groupChatId: "Test-GroupId",
+        id: "Test-MessageId1",
+        imageUrl: null,
+        receiverId: null,
+        senderId: "Test-UserAId",
+        sender: {
+          id: "Test-UserAId",
+          imageUrl: null,
+          username: "Test-UserAUsername",
+        },
+      },
+      {
+        content: "Test-MessageContent2",
+        createdAt: "2020-01-01T01:30:00Z",
+        groupChatId: "Test-GroupId",
+        id: "Test-MessageId2",
+        imageUrl: "Test-MessageImageUrl",
+        receiverId: null,
+        senderId: "Test-UserBId",
+        sender: {
+          id: "Test-UserBId",
+          imageUrl: "Test-UserBImageUrl",
+          username: "Test-UserBUsername",
+        },
+      },
+    ],
+  };
+
+  const mockCurrentUser: User = {
+    id: "Test-UserAId",
+    imageUrl: null,
+    username: "Test-UserAUsername",
+    isGuest: false,
+    lastSeen: "2020-01-01T01:30:00Z",
+  };
+
   beforeAll(() => {
     fetchMock.mockGlobal();
     manageFetchMockGlobally();
+  });
+
+  beforeEach(() => {
+    vi.spyOn(jwtTokenService, "getJwtToken").mockReturnValue("jwtToken");
+    fetchMock.get("/auth/get-user", {
+      status: 200,
+      body: {
+        user: mockCurrentUser,
+      },
+    });
   });
 
   afterEach(() => {
@@ -48,14 +121,14 @@ describe("group-chat-page component", () => {
       expect.hasAssertions();
 
       vi.spyOn(console, "error").mockImplementation(() => null);
-      fetchMock.get(serverGetGroupChat, {
+      fetchMock.get(serverGroupMessagesRoute, {
         status: 500,
         body: {
           error: "Server error!",
         },
       });
       const router = createMemoryRouter(routes, {
-        initialEntries: ["/groups/groupId"],
+        initialEntries: ["/groups/Test-GroupId"],
       });
       renderWithProviders(<RouterProvider router={router} />);
 
@@ -79,23 +152,95 @@ describe("group-chat-page component", () => {
           closeSnackbar: vi.fn<() => void>(),
         };
       });
-      fetchMock.get(serverGetGroupChat, {
+      fetchMock.get(serverGroupMessagesRoute, {
         status: 404,
         body: {
           errors: [{ message: "Group not found." }],
         } as ApiClientError,
       });
       const router = createMemoryRouter(routes, {
-        initialEntries: ["/groups/groupId"],
+        initialEntries: ["/groups/Test-GroupId"],
       });
 
       renderWithProviders(<RouterProvider router={router} />);
-      await waitForElementToBeRemoved(screen.getByTestId("loader"));
+      const indexPageElement = await screen.findByText("Index Page");
 
       expect(mockEnqueueSnackbar).toHaveBeenCalledWith<
         [notistack.SnackbarMessage, notistack.OptionsObject]
       >("Group not found.", { variant: "error" });
       expect(router.state.location.pathname).toBe("/");
+      expect(indexPageElement).toBeInTheDocument();
     });
+  });
+
+  describe("render group messages data", () => {
+    it("should render group name as a link", async () => {
+      expect.hasAssertions();
+
+      fetchMock.get(serverGroupMessagesRoute, {
+        status: 200,
+        body: mockGroupMessages,
+      });
+      const router = createMemoryRouter(routes, {
+        initialEntries: ["/groups/Test-GroupId"],
+      });
+      renderWithProviders(<RouterProvider router={router} />);
+
+      const groupNameLink = await screen.findByRole<HTMLAnchorElement>("link", {
+        name: "Test-GroupName",
+      });
+
+      expect(groupNameLink).toBeInTheDocument();
+      expect(groupNameLink.pathname).toBe("/groups/Test-GroupId/details");
+    });
+
+    it("should navigate to group details page when group name link is clicked", async () => {
+      expect.hasAssertions();
+
+      fetchMock.get(serverGroupMessagesRoute, {
+        status: 200,
+        body: mockGroupMessages,
+      });
+      const router = createMemoryRouter(routes, {
+        initialEntries: ["/groups/Test-GroupId"],
+      });
+      renderWithProviders(<RouterProvider router={router} />);
+
+      const groupNameLink = await screen.findByRole("link", {
+        name: "Test-GroupName",
+      });
+      await userEvent.click(groupNameLink);
+      const groupDetailsPage = screen.getByText("Group Details Page");
+
+      expect(router.state.location.pathname).toBe(
+        "/groups/Test-GroupId/details",
+      );
+      expect(groupDetailsPage).toBeInTheDocument();
+    });
+
+    it("should render group messages content", async () => {
+      expect.hasAssertions();
+
+      fetchMock.get(serverGroupMessagesRoute, {
+        status: 200,
+        body: mockGroupMessages,
+      });
+      const router = createMemoryRouter(routes, {
+        initialEntries: ["/groups/Test-GroupId"],
+      });
+      renderWithProviders(<RouterProvider router={router} />);
+
+      const firstGroupMessageContent = await screen.findByText(
+        "Test-MessageContent1",
+      );
+      const secondGroupMessageContent = await screen.findByText(
+        "Test-MessageContent2",
+      );
+
+      expect(firstGroupMessageContent).toBeInTheDocument();
+      expect(secondGroupMessageContent).toBeInTheDocument();
+    });
+
+    it.todo("should render group messages images");
   });
 });
