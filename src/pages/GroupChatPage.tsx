869f909399966e94d95dfeb4160f71d1fee05475
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-04-01 08:01:40 +0200

    Feat: fetch group messages and handle any errors

--- src/pages/GroupChatPage.tsx
+++ src/pages/GroupChatPage.tsx
@@ -1,3 +1,40 @@
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router";
+import { useSnackbar } from "notistack";
+import { useGetGroupMessagesQuery } from "@/slices/groupsSlice";
+import handleUnexpectedError from "@/utils/handleUnexpectedError";
+import Loader from "@/components/Loader/Loader";
+import { isClientError, isFetchBaseQueryError } from "@/types/apiResponseTypes";
+
+function assert(value: unknown): asserts value {
+  if (!value) {
+    throw new Error("Value is undefined");
+  }
+}
+
 export default function GroupChatPage() {
+  const { groupId } = useParams<"groupId">();
+  assert(groupId);
+  const { isError, isLoading, error } = useGetGroupMessagesQuery(groupId);
+  const { enqueueSnackbar } = useSnackbar();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isError) {
+      if (isFetchBaseQueryError(error) && isClientError(error.data)) {
+        error.data.errors.forEach((error) => {
+          enqueueSnackbar(error.message, { variant: "error" });
+        });
+        void navigate("/", { replace: true });
+      } else {
+        handleUnexpectedError(error);
+      }
+    }
+  }, [isError, error, enqueueSnackbar, navigate]);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return <p>GROUPS CHAT PAGE</p>;
 }
