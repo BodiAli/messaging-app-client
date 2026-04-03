0aa22f6eeb03468743a09a057f50cdb430a8c0f7
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-04-03 10:40:52 +0200

    Feat: handle submitting message to group chat

--- src/pages/GroupChatPage.tsx
+++ src/pages/GroupChatPage.tsx
@@ -1,14 +1,23 @@
-import { useEffect } from "react";
+import {
+  useEffect,
+  type Dispatch,
+  type SetStateAction,
+  type SubmitEvent,
+} from "react";
 import { useNavigate, useParams, Link as RouterLink } from "react-router";
 import { Box, Link } from "@mui/material";
 import { useSnackbar } from "notistack";
-import { useGetGroupMessagesQuery } from "@/slices/groupsSlice";
+import {
+  useGetGroupMessagesQuery,
+  useSendGroupMessageMutation,
+} from "@/slices/groupsSlice";
 import handleUnexpectedError from "@/utils/handleUnexpectedError";
 import Loader from "@/components/Loader/Loader";
 import { isClientError, isFetchBaseQueryError } from "@/types/apiResponseTypes";
 import Chatting from "@/components/Chatting";
 import { useAppSelector } from "@/app/hooks";
 import { selectUser } from "@/slices/authSlice";
+import SendMessage from "@/components/SendMessage";
 
 function assert(value: unknown): asserts value {
   if (!value) {
@@ -16,32 +25,98 @@ function assert(value: unknown): asserts value {
   }
 }
 
+interface FormFields extends HTMLFormControlsCollection {
+  messageContent: HTMLInputElement;
+  messageImage: HTMLInputElement;
+}
+
+interface FormWithElements extends HTMLFormElement {
+  elements: FormFields;
+}
+
 export default function GroupChatPage() {
   const { groupId } = useParams<"groupId">();
   assert(groupId);
-  const { data, isError, isLoading, isFetching, error } =
-    useGetGroupMessagesQuery(groupId);
   const { enqueueSnackbar } = useSnackbar();
   const navigate = useNavigate();
+  const {
+    data,
+    isError: isGetMessageError,
+    isLoading: isGetMessagesLoading,
+    isFetching,
+    error: getMessageError,
+  } = useGetGroupMessagesQuery(groupId);
+  const [
+    sendMessage,
+    {
+      isLoading: isSendingMessage,
+      isError: isMessageError,
+      error: messageError,
+    },
+  ] = useSendGroupMessageMutation();
   const currentUser = useAppSelector(selectUser);
   assert(currentUser);
 
   useEffect(() => {
-    if (isError) {
-      if (isFetchBaseQueryError(error) && isClientError(error.data)) {
-        error.data.errors.forEach((error) => {
+    if (isGetMessageError) {
+      if (
+        isFetchBaseQueryError(getMessageError) &&
+        isClientError(getMessageError.data)
+      ) {
+        getMessageError.data.errors.forEach((error) => {
           enqueueSnackbar(error.message, { variant: "error" });
         });
         void navigate("/", { replace: true });
       } else {
-        handleUnexpectedError(error);
+        handleUnexpectedError(getMessageError);
       }
     }
-  }, [isError, error, enqueueSnackbar, navigate]);
+  }, [isGetMessageError, getMessageError, enqueueSnackbar, navigate]);
 
-  if (isLoading || !data) {
+  if (isGetMessagesLoading || !data) {
     return <Loader />;
   }
+
+  if (isMessageError) {
+    if (
+      isFetchBaseQueryError(messageError) &&
+      isClientError(messageError.data)
+    ) {
+      messageError.data.errors.forEach((error) => {
+        enqueueSnackbar(error.message, {
+          variant: "error",
+        });
+      });
+    } else {
+      handleUnexpectedError(messageError);
+    }
+  }
+
+  const handleMessageSubmit = (
+    setUploadedImageUrl: Dispatch<SetStateAction<string | null>>,
+  ) => {
+    return async function (e: SubmitEvent<FormWithElements>) {
+      e.preventDefault();
+
+      const form = e.currentTarget;
+      const formData = new FormData(form);
+      const messageContent = form.elements.messageContent.value;
+      const messageImage = form.elements.messageImage.files?.[0];
+
+      if (messageContent.trim().length === 0) {
+        alert("Cannot send an empty message");
+        return;
+      }
+
+      if (messageImage) {
+        formData.append("messageImage", messageContent);
+      }
+
+      await sendMessage({ groupId, formData });
+      form.reset();
+      setUploadedImageUrl(null);
+    };
+  };
 
   return (
     <Box
@@ -81,6 +156,10 @@ export default function GroupChatPage() {
         currentUserId={currentUser.id}
         isFetching={isFetching}
         messages={data.messages}
+      />
+      <SendMessage
+        isLoading={isSendingMessage}
+        onSubmit={handleMessageSubmit}
       />
     </Box>
   );
