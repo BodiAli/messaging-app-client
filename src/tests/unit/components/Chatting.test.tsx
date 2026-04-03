24c7753e18142305dddc928ffad141be55a9fd20
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-04-03 04:16:30 +0200

    Feat: test rendering group messages

--- src/tests/unit/components/Chatting.test.tsx
+++ src/tests/unit/components/Chatting.test.tsx
@@ -3,7 +3,7 @@ import { screen } from "@testing-library/react";
 import { createRoutesStub } from "react-router";
 import renderWithProviders from "@/utils/test-utils";
 import Chatting from "@/components/Chatting";
-import type { Messages } from "@/types/modelsType";
+import type { GroupMessages, Messages } from "@/types/modelsType";
 
 vi.mock(import("@/components/SendMessage"), () => {
   return {
@@ -14,7 +14,7 @@ vi.mock(import("@/components/SendMessage"), () => {
 });
 
 describe("chatting component", () => {
-  const mockMessages: Messages = [
+  const mockUsersMessages: Messages = [
     {
       content: "messageFromUserA",
       createdAt: new Date("2020-01-01T01:30:00").toISOString(),
@@ -35,7 +35,40 @@ describe("chatting component", () => {
     },
   ];
 
-  function renderChattingComponent(messages: Messages) {
+  const mockGroupMessages: GroupMessages["messages"] = [
+    {
+      content: "Test-Message content 1",
+      createdAt: "2020-01-01T01:30:30",
+      groupChatId: "Test-GroupId",
+      id: "Test-MessageId",
+      imageUrl: "Test-ImageUrl",
+      receiverId: null,
+      senderId: "Test-UserAId",
+      sender: {
+        id: "Test-UserAId",
+        imageUrl: "Test-ImageUrl",
+        username: "Test-User A username",
+      },
+    },
+    {
+      content: "Test-Message content 2",
+      createdAt: "2020-01-02T02:30:30",
+      groupChatId: "Test-GroupId",
+      id: "Test-MessageId1",
+      imageUrl: null,
+      receiverId: null,
+      senderId: "Test-UserBId",
+      sender: {
+        id: "Test-UserBId",
+        imageUrl: null,
+        username: "Test-User B username",
+      },
+    },
+  ];
+
+  function renderChattingComponent(
+    messages: Messages | GroupMessages["messages"],
+  ) {
     const Stub = createRoutesStub([
       {
         path: "/:userId",
@@ -51,58 +84,125 @@ describe("chatting component", () => {
     return renderWithProviders(<Stub initialEntries={["/userId"]} />);
   }
 
-  describe("rendering messages", () => {
-    describe("given no messages", () => {
-      it("should render no messages", () => {
-        expect.hasAssertions();
+  describe("rendering empty messages", () => {
+    it("should render no messages", () => {
+      expect.hasAssertions();
 
-        renderChattingComponent([]);
+      renderChattingComponent([]);
 
-        const messages = screen.queryAllByText(/^messageFromUser/);
+      const messages = screen.queryAllByText(/^messageFromUser/);
 
-        expect(messages).toHaveLength(0);
-      });
+      expect(messages).toHaveLength(0);
+    });
+  });
+
+  describe("rendering users messages array", () => {
+    it("should render message content", () => {
+      expect.hasAssertions();
+
+      renderChattingComponent(mockUsersMessages);
+
+      const firstMessage = screen.getByText("messageFromUserA");
+      const secondMessage = screen.getByText("messageFromUserB");
+
+      expect(firstMessage).toHaveTextContent("messageFromUserA");
+      expect(secondMessage).toHaveTextContent("messageFromUserB");
     });
 
-    describe("given messages array", () => {
-      it("should render message content", () => {
-        expect.hasAssertions();
+    it("should render message image when imageUrl is not null", () => {
+      expect.hasAssertions();
 
-        renderChattingComponent(mockMessages);
+      renderChattingComponent(mockUsersMessages);
 
-        const firstMessage = screen.getByText("messageFromUserA");
-        const secondMessage = screen.getByText("messageFromUserB");
-
-        expect(firstMessage).toHaveTextContent("messageFromUserA");
-        expect(secondMessage).toHaveTextContent("messageFromUserB");
+      const messageImage = screen.getByRole("img", {
+        name: "user sent image",
       });
 
-      it("should render message image when imageUrl is not null", () => {
-        expect.hasAssertions();
+      expect(messageImage).toBeInTheDocument();
+    });
 
-        renderChattingComponent(mockMessages);
+    it("should render when was the message created", () => {
+      expect.hasAssertions();
 
-        const messageImage = screen.getByRole("img", {
-          name: "user sent image",
-        });
+      renderChattingComponent(mockUsersMessages);
 
-        expect(messageImage).toBeInTheDocument();
+      const messagesTime = screen.getAllByRole("time");
+      assert(messagesTime[0]);
+      assert(messagesTime[1]);
+      const firstMessageTime = messagesTime[0];
+      const secondMessageTime = messagesTime[1];
+
+      expect(firstMessageTime).toHaveTextContent("Jan 1, 2020, 01:30 AM");
+      expect(secondMessageTime).toHaveTextContent("Jan 2, 2020, 02:00 AM");
+    });
+  });
+
+  describe("rendering group messages array", () => {
+    it("should render message content", () => {
+      expect.hasAssertions();
+
+      renderChattingComponent(mockGroupMessages);
+
+      const firstGroupMessage = screen.getByText("Test-Message content 1");
+      const secondGroupMessage = screen.getByText("Test-Message content 2");
+
+      expect(firstGroupMessage).toBeInTheDocument();
+      expect(secondGroupMessage).toBeInTheDocument();
+    });
+
+    it("should render message image when imageUrl is not null", () => {
+      expect.hasAssertions();
+
+      renderChattingComponent(mockGroupMessages);
+
+      const messageImage = screen.getByRole("img", {
+        name: "user sent image",
       });
 
-      it("should render when was the message created", () => {
-        expect.hasAssertions();
+      expect(messageImage).toBeInTheDocument();
+    });
 
-        renderChattingComponent(mockMessages);
+    it("should render when was the message created", () => {
+      expect.hasAssertions();
 
-        const messagesTime = screen.getAllByRole("time");
-        assert(messagesTime[0]);
-        assert(messagesTime[1]);
-        const firstMessageTime = messagesTime[0];
-        const secondMessageTime = messagesTime[1];
+      renderChattingComponent(mockGroupMessages);
 
-        expect(firstMessageTime).toHaveTextContent("Jan 1, 2020, 01:30 AM");
-        expect(secondMessageTime).toHaveTextContent("Jan 2, 2020, 02:00 AM");
+      const messagesTime = screen.getAllByRole("time");
+      assert(messagesTime[0]);
+      assert(messagesTime[1]);
+      const firstMessageTime = messagesTime[0];
+      const secondMessageTime = messagesTime[1];
+
+      expect(firstMessageTime).toHaveTextContent("Jan 1, 2020, 01:30 AM");
+      expect(secondMessageTime).toHaveTextContent("Jan 2, 2020, 02:30 AM");
+    });
+
+    it("should render message sender profile picture", () => {
+      expect.hasAssertions();
+
+      renderChattingComponent(mockGroupMessages);
+
+      const userAProfilePicture = screen.getByRole("img", {
+        name: "Test-User A username's profile picture",
       });
+      const userBProfilePicture = screen.getByTitle(
+        "Test-User B username no profile picture",
+      );
+
+      expect(userAProfilePicture).toBeInTheDocument();
+      expect(userBProfilePicture).toBeInTheDocument();
+    });
+
+    it("should render message sender username", () => {
+      expect.hasAssertions();
+
+      renderChattingComponent(mockGroupMessages);
+
+      const userAUsername = screen.getByText("Test-User A username");
+      const userBUsername = screen.getByText("Test-User B username");
+
+      expect(userAUsername).toBeInTheDocument();
+      expect(userBUsername).toBeInTheDocument();
     });
   });
 });
