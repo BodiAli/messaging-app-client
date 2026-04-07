f75b0b1dede6dd2ddf661005abd0a0e51575d42a
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-04-07 05:23:08 +0200

    Feat: initialize fetching group details

--- src/pages/GroupDetailsPage.tsx
+++ src/pages/GroupDetailsPage.tsx
@@ -1,5 +1,26 @@
-// TODO: Implement.
+import { useParams } from "react-router";
+import { Box } from "@mui/material";
+import { useGetGroupDetailsQuery } from "@/slices/groupsSlice";
+import handleUnexpectedError from "@/utils/handleUnexpectedError";
 
-export default function GroupDetails() {
-  return <p>GROUP DETAILS</p>;
+export default function GroupDetailsPage() {
+  const { groupId } = useParams<"groupId">();
+  assert(groupId);
+  const { isError, error } = useGetGroupDetailsQuery(groupId);
+
+  if (isError) {
+    handleUnexpectedError(error);
+  }
+
+  return (
+    <Box>
+      <p>GROUP DETAILS</p>;
+    </Box>
+  );
+}
+
+function assert(value: unknown): asserts value {
+  if (!value) {
+    throw new Error("Value is not defined");
+  }
 }
