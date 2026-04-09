11eb8a5a984c1256a652816fb89914afd683d901
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-04-09 14:31:33 +0200

    Feat: implement handling sending group invites to non-member friends

--- src/pages/GroupDetailsPage.tsx
+++ src/pages/GroupDetailsPage.tsx
@@ -1,20 +1,123 @@
-import { useParams } from "react-router";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router";
 import { Box } from "@mui/material";
-import { useGetGroupDetailsQuery } from "@/slices/groupsSlice";
+import { useSnackbar } from "notistack";
+import {
+  useGetGroupDetailsQuery,
+  useSendGroupInviteMutation,
+} from "@/slices/groupsSlice";
 import handleUnexpectedError from "@/utils/handleUnexpectedError";
+import {
+  isClientError,
+  isFetchBaseQueryError,
+  isServerError,
+} from "@/types/apiResponseTypes";
+import GroupDetailsHeader from "@/components/GroupDetailsHeader";
+import Loader from "@/components/Loader/Loader";
+import { useAppSelector } from "@/app/hooks";
+import { selectUser } from "@/slices/authSlice";
+import { useGetFriendsQuery } from "@/slices/friendsSlice";
 
 export default function GroupDetailsPage() {
+  const [fatalError, setFatalError] = useState<string | null>(null);
   const { groupId } = useParams<"groupId">();
   assert(groupId);
-  const { isError, error } = useGetGroupDetailsQuery(groupId);
+  const { enqueueSnackbar } = useSnackbar();
+  const navigate = useNavigate();
+  const {
+    data: userFriends,
+    isError: isUserFriendsError,
+    isLoading: isUserFriendsLoading,
+    error: userFriendsError,
+  } = useGetFriendsQuery(undefined);
+  const {
+    data: groupDetails,
+    isError: isGroupDetailsError,
+    error: groupDetailsError,
+    isLoading: isGroupDetailsLoading,
+  } = useGetGroupDetailsQuery(groupId);
+  const [sendGroupInvite, { isLoading: isSendGroupInviteLoading }] =
+    useSendGroupInviteMutation();
+  const currentUser = useAppSelector(selectUser);
+  assert(currentUser);
 
-  if (isError) {
-    handleUnexpectedError(error);
+  const handleGroupInvite = async (friendIds: string[]) => {
+    try {
+      await sendGroupInvite({ friendIds, groupId }).unwrap();
+      enqueueSnackbar("Group invitation sent.", { variant: "success" });
+    } catch (error) {
+      if (isFetchBaseQueryError(error) && isClientError(error.data)) {
+        error.data.errors.forEach((error) => {
+          enqueueSnackbar(error.message, {
+            variant: "error",
+          });
+        });
+        return;
+      }
+      if (isFetchBaseQueryError(error) && isServerError(error.data)) {
+        setFatalError(error.data.error);
+        return;
+      }
+
+      if (Error.isError(error)) {
+        setFatalError(error.message);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (isGroupDetailsError) {
+      if (
+        isFetchBaseQueryError(groupDetailsError) &&
+        isClientError(groupDetailsError.data)
+      ) {
+        groupDetailsError.data.errors.forEach((error) => {
+          enqueueSnackbar(error.message, {
+            variant: "error",
+          });
+        });
+        void navigate("/");
+      } else {
+        handleUnexpectedError(groupDetailsError);
+      }
+    }
+  }, [isGroupDetailsError, groupDetailsError, navigate, enqueueSnackbar]);
+
+  if (fatalError) {
+    throw new Error(fatalError);
   }
+
+  if (isUserFriendsError) {
+    handleUnexpectedError(userFriendsError);
+  }
+
+  if (
+    isGroupDetailsLoading ||
+    !groupDetails ||
+    isUserFriendsLoading ||
+    !userFriends
+  ) {
+    return <Loader />;
+  }
+
+  const {
+    group: { users, ...group },
+  } = groupDetails;
+
+  const nonMemberUsers = userFriends.friends.filter(
+    (_friend, index) => !users[index],
+  );
 
   return (
     <Box>
-      <p>GROUP DETAILS</p>;
+      <GroupDetailsHeader
+        group={group}
+        currentUserId={currentUser.id}
+        nonMemberUsers={nonMemberUsers}
+        onGroupInvite={handleGroupInvite}
+        isSendingInvite={isSendGroupInviteLoading}
+      />
+      ;
     </Box>
   );
 }
